945cb27c159662c201694b83c94f0ffb9c4387c6
David López Albajara <dlalbajara@gmail.com>
2021-02-01 17:05:16 +0100

    Add: exercise 2.2

--- part2/course-info/src/components/Total.js
+++ part2/course-info/src/components/Total.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises
-  return <p>Number of exercises {sum}</p>
+  let sum = 0
+  for (let part of course.parts) {
+    sum += part.exercises
+  }
+  // const sum =
+  //   course.parts[0].exercises +
+  //   course.parts[1].exercises +
+  //   course.parts[2].exercises
+  return <strong>Total Number of exercises: {sum}</strong>
 }
 
 export default Total
