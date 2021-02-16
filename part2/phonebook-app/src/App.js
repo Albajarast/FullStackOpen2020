642b2b3eab301e152d9a06c2e2d4ea1680c22435
David López Albajara <dlalbajara@gmail.com>
2021-02-16 10:56:03 +0100

    Add: exercise 2.11. Fetch data from json-server

--- part2/phonebook-app/src/App.js
+++ part2/phonebook-app/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import FilterName from './components/FilterName'
 import PersonForm from './components/PersonForm'
 import ContactsList from './components/ContactsList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+
+  const fetchData = () => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(fetchData, [])
 
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(newSearch.toLowerCase())
